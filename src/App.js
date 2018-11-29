55343896586998f918b627c37982522a76f3fbb4
Irving Andrade <irving.an.ve@gmail.com>
2018-11-28 20:31:41 -0600

    Add table of search

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
 import SearchDisplay from './components/search/SearchDisplay';
+import TableDisplay from './components/table/TableDisplay'
+
 import axios from 'axios';
 
 class App extends Component {
   
   state={
-    form:{}
+    form:{},
+    data:[]
   }
 
-  onSubmit=(e)=>{
+  handleSubmit=(e)=>{
     e.preventDefault();
-    const form = this.state;
-    console.log(form)
+    const {form} = this.state;
+    if(!form.type || !form.search)return alert("Te falto rellenar un campo");
+    const endPoint =`http://musicbrainz.org/ws/2/${form.type}/?query=${form.search}&fmt=json`;
+    axios.get(endPoint)
+    .then(res=>{
+      const apiResponse =res.data.artists || res.data.releases || res.data.recordings;
+      console.log(apiResponse)
+      this.setState({data:apiResponse})
+    })
+    .catch(error=>console.log(error))
   }
 
-  onChange=(e)=>{
+  handleChange=(e)=>{
     const {form} = this.state;
     const key = e.target.name; 
     const value = e.target.value;
     form[key]=value;
-    console.log(form)
+    this.setState({form})
   }
 
   render() {
     return (
       <div>
         <SearchDisplay
-        onChange={this.onChange}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
         />
+        {this.state.data.length===0?'':
+        <TableDisplay 
+        data={this.state.data}
+        />
+        }
       </div>
     );
   }
--- src/components/search/SearchDisplay.jsx
+++ src/components/search/SearchDisplay.jsx
@@ -4,21 +4,28 @@ import {TextField,Grid,Button} from '@material-ui/core'
 const SearchDisplay = (props) => {
     return (
         <div>
-            <form onSubmit={props.onSubmit}> 
+            <form onSubmit={props.handleSubmit}> 
                 <Grid container spacing={8} justify={'center'} alignItems={'center'}>
                     <Grid item>
                         <TextField
-                        onChange={props.onChange}
+                        onChange={props.handleChange}
                         name="search"
                         />
                     </Grid>
                     <Grid item>
                         <TextField
                         select
-                        onChange={props.onChange}
+                        onChange={props.handleChange}
                         name="type"
-                        value='hola'
-                        />
+                        SelectProps={{
+                            native: true 
+                        }}
+                        >
+                            <option value="" selected disabled hidden>Selecciona una categoría</option>
+                            <option value="artist">Artista</option>
+                            <option value="recording">Cancion</option>
+                            <option value="release">Album</option>
+                        </TextField>
                     </Grid>
                     <Grid item>
                         <Button variant="contained" type="submit">Buscar</Button>
